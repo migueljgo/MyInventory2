3ad45d7eeb2e5ff0a61fecea4a06fb0dbd1888d4
migueljgo <gomjbausa@gmail.com>
2021-03-23 22:07:33 +0800

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyInventory2.Data;
+using MyInventory2.Models;
+
+[assembly: HostingStartup(typeof(MyInventory2.Areas.Identity.IdentityHostingStartup))]
+namespace MyInventory2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
